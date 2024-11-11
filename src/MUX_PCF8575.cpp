e65ccd09ca9fa79b758c6646385ff17a41bd6884
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2024-11-11 23:02:56 +0000

    Update MUX_PCF8575.cpp
    Corregido algunos errores funcionamiento para ESP32

--- src/MUX_PCF8575.cpp
+++ src/MUX_PCF8575.cpp
@@ -1,49 +1,73 @@
-#include "MUX_PCF8575.h"
+#include <MUX_PCF8575.h>
 #include "Arduino.h"
 
-MUX_PCF8575::MUX_PCF8575(uint8_t ADDR_I2C, uint16_t estadoInicialPines) {
+MUX_PCF8575::MUX_PCF8575(uint8_t ADDR_I2C, uint16_t estadoInicialPines)
+{
   _ADDR_I2C = ADDR_I2C;
-  _estadoPines = estadoInicialPines;  // Inicialmente, todos los pines están en LOW
+  _estadoPines = estadoInicialPines; // Inicialmente, todos los pines están en LOW
 }
 
-void MUX_PCF8575::begin() {
-  Wire.begin();
-  sendData(0xFFFF); // Inicializar todos como entrada
+bool MUX_PCF8575::begin()
+{
+  // Prueba de comunicación con el dispositivo
+  Wire.beginTransmission(_ADDR_I2C);
+  uint8_t result = Wire.endTransmission();
+  if (result != 0) return false;  // Retorna false si la inicialización falla
+
+  sendData(0xFFFF);  // Inicializar todos como entrada
   sendData(_estadoPines);
+
+  return true;  // Retorna true si la inicialización es exitosa
 }
 
-void MUX_PCF8575::pinMode(uint8_t pin, uint8_t mode) {
-  if (mode == OUTPUT) {
+void MUX_PCF8575::pinMode(uint8_t pin, uint8_t mode)
+{
+  if (mode == OUTPUT)
+  {
     _estadoPines &= ~(1 << pin); // Establece el pin como salida
-
-  } else if (mode == INPUT) {
+  }
+  else if (mode == INPUT)
+  {
     _estadoPines &= ~(1 << pin); // Desactiva la resistencia pull-up interna
-  } else if (mode == INPUT_PULLUP) {
+  }
+  else if (mode == INPUT_PULLUP)
+  {
     _estadoPines |= (1 << pin); // Habilita la resistencia pull-up interna
   }
   sendData(_estadoPines);
 }
 
-void MUX_PCF8575::digitalWrite(uint8_t pin, bool value) {
-  if (value == HIGH) {
-    _estadoPines |= (1 << pin);  // Establece el pin en HIGH
-  } else {
-    _estadoPines &= ~(1 << pin);  // Establece el pin en LOW
+void MUX_PCF8575::digitalWrite(uint8_t pin, bool value)
+{
+  if (value == HIGH)
+  {
+    _estadoPines |= (1 << pin); // Establece el pin en HIGH
+  }
+  else
+  {
+    _estadoPines &= ~(1 << pin); // Establece el pin en LOW
   }
   sendData(_estadoPines);
 }
 
-void MUX_PCF8575::sendData (uint16_t value) {
+void MUX_PCF8575::sendData(uint16_t value)
+{
   Wire.beginTransmission(_ADDR_I2C);
   Wire.write(lowByte(value));
   Wire.write(highByte(value));
   Wire.endTransmission();
+  delayMicroseconds(10);
 }
 
-bool MUX_PCF8575::digitalRead(uint8_t pin) {
+bool MUX_PCF8575::digitalRead(uint8_t pin)
+{
   Wire.beginTransmission(_ADDR_I2C);
-  Wire.requestFrom(_ADDR_I2C, (uint8_t)2);
-  uint16_t data = Wire.read();
-  data |= (Wire.read() << 8);
-  return (data >> pin) & 0x01;
+  Wire.endTransmission(); // Termina la transmisión y obtiene el resultado
+  delayMicroseconds(10);
+
+  Wire.requestFrom(_ADDR_I2C, (uint8_t)2); // Solicita 2 bytes desde la dirección I2C del dispositivo
+  delayMicroseconds(10);
+  uint16_t data = Wire.read();             // Lee el primer byte
+  data |= (Wire.read() << 8);              // Lee el segundo byte y lo desplaza para formar un uint16_t
+  return (data >> pin) & 0x01;             // Retorna el estado del pin solicitado
 }
