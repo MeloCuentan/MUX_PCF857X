46f267b37b27166697b1783ef114c9a94320c3fa
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2025-01-18 16:29:35 +0000

    Update MUX_PCF8575.h

--- src/MUX_PCF8575.h
+++ src/MUX_PCF8575.h
@@ -1,21 +1,34 @@
-#ifndef MUX_PCF8575_h
-#define MUX_PCF8575_h
+#ifndef MUX_PCF_h
+#define MUX_PCF_h
 
 #include <Wire.h>
+#include <Arduino.h>
 
-class MUX_PCF8575 {
+enum MUX_Type
+{ // Tipos de multiplexores
+  PCF8574,
+  PCF8575
+};
+
+class MUX_PCF
+{
 public:
-  MUX_PCF8575(uint8_t ADDR_I2C);
+  MUX_PCF(uint8_t ADDR_I2C = 0x20, MUX_Type type = PCF8574, TwoWire *wire = &Wire); // Constructor
 
-  bool begin(uint16_t estadoInicialPines = 0x0000);
-  void pinMode(uint8_t pin, uint8_t mode);
-  void digitalWrite(uint8_t pin, bool value);
-  bool digitalRead(uint8_t pin);
+  bool begin(uint16_t estadoInicialPines = 0x0000); // Iniciar el multiplexor
+  void pinMode(uint8_t pin, uint8_t mode);          // Establecer el modo de un pin
+  void digitalWrite(uint8_t pin, bool value);       // Escribir en un pin
+  bool digitalRead(uint8_t pin);                    // Leer un pin
+
+  uint16_t getPinState();           // Obtener el estado de los pines
+  void setPinState(uint16_t value); // Establecer el estado de los pines
 
 private:
-  uint8_t _ADDR_I2C;
-  uint16_t _estadoPines;
-  void sendData(uint16_t value);
+  uint8_t _ADDR_I2C;             // Dirección I2C del multiplexor
+  uint16_t _estadoPines;         // Estado de los pines
+  MUX_Type _type;                // Tipo de multiplexor
+  TwoWire *_wire;                // Puntero al bus I2C
+  void sendData(uint16_t value); // Enviar datos al multiplexor
 };
 
 #endif
