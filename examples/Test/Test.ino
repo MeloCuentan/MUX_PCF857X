650ddad9934cad146d2370ef65d3e823f2fddc60
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2025-01-18 16:37:13 +0000

    Update Test.ino

--- examples/Test/Test.ino
+++ examples/Test/Test.ino
@@ -4,9 +4,9 @@ const uint8_t ADDR_PCF = 0x20;  // Asignamos la dirección I2C del multiplexor
 
 MUX_PCF8575 pcf(ADDR_PCF);  // Creamos el objeto con la dirección I2C
 
-unsigned long tiempoActual, tiempoAnterior; // Estas variables serán para el control del funcionamiento
-unsigned long intervalo = 500;  // Cada vez que se pase ese tiempo, habrá un cambio en los pines de salida
-int numeroPin = 0;
+uint32_t tiempoActual, tiempoAnterior; // Estas variables serán para el control del funcionamiento
+uint32_t intervalo = 500;  // Cada vez que se pase ese tiempo, habrá un cambio en los pines de salida
+uint8_t numeroPin = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -14,19 +14,19 @@ void setup() {
   pcf.begin();  // Inicializamos el objeto
 
   // *************  comprobar pines como salida ***************
-  for (int i = 0; i < 8; i++) {  // Creamos un bucle para recorrer los pines de P0 a P07
+  for (uint8_t i = 0; i < 8; i++) {  // Creamos un bucle para recorrer los pines de P0 a P07
     pcf.pinMode(i, OUTPUT);      // Configurar los pines como salida
   }
 
   // ************  comprobar pines como entradas **************
-  for (int i = 8; i < 16; i++) {  // Creamos un bucle para recorrer los pines de P10 a P17 (corresponden del 8 al 15)
+  for (uint8_t i = 8; i < 16; i++) {  // Creamos un bucle para recorrer los pines de P10 a P17 (corresponden del 8 al 15)
     pcf.pinMode(i, INPUT_PULLUP);        // Configuramos los pines como entrada
   }
 }
 
 void loop() {
   tiempoActual = millis();
-  for (int i = 8; i < 16; i++) {
+  for (uint8_t i = 8; i < 16; i++) {
     if (pcf.digitalRead(i) == LOW) {
       Serial.println("Pulsado el pin " + String(i));
     }
