40634feebc6b9f9ff7fce1987a51eee946eb920b
Luiti_LP <89021748+MeloCuentan@users.noreply.github.com>
2025-01-19 00:40:09 +0000

    Update newFunctions.ino

--- examples/newFunctions/newFunctions.ino
+++ examples/newFunctions/newFunctions.ino
@@ -1,43 +1,60 @@
-#include "MUX_PCF857X.h"  // Agregamos la librería
+#include <Wire.h>
+#include <MUX_PCF857X.h>
 
-const uint8_t ADDR_PCF = 0x20;  // Asignamos la dirección I2C del multiplexor
+void leerPulsadores(); // Leer todas las entradas del PCF8574 y la del interruptor externo
 
-MUX_PCF pcf(ADDR_PCF, PCF8575, &Wire);  // Creamos el objeto con la dirección I2C, el tipo de PCF y el bus I2C que se utilizará
+const uint8_t ADDR_PCF = 0x20;
+const uint8_t cantidadEntradas = 16;              // Cantidad de encoders
+const uint8_t pinSDA = 23;                        // Pin SDA del I2C_OUT
+const uint8_t pinSCL = 19;                        // Pin SCL del I2C_OUT
+bool valorPulsadores[cantidadEntradas] = {false}; // Valores de los pulsadores
 
-uint32_t tiempoActual, tiempoAnterior; // Estas variables serán para el control del funcionamiento
-uint32_t intervalo = 500;  // Cada vez que se pase ese tiempo, habrá un cambio en los pines de salida
-uint8_t numeroPin = 0;
+uint32_t tiempoActual;           // Variable de tiempo actual del programa
+uint32_t tiempoAnterior;         // Variable de tiempo anterior del programa
+const uint32_t intervalo = 1000; // Intervalo de tiempo para la lectura de los pulsadores
+uint32_t tiempoInicio;           // Variables para el control del funcionamiento
+uint32_t tiempoFinal;            // Variables para el control del funcionamiento
 
-void setup() {
-  Serial.begin(115200);
-  Wire.begin(SDA, SCL, 100000);  // Inicializamos el bus I2C
-  pcf.begin(0x00FF);  // Inicializamos el objeto
+MUX_PCF pcf(ADDR_PCF, PCF8575, &Wire1); // Multiplexor PCF8575 en la dirección 0x20 y en el puerto I2C adicional
 
-  // *************  comprobar pines como salida ***************
-  for (uint8_t i = 0; i < 8; i++) {  // Creamos un bucle para recorrer los pines de P0 a P07
-    pcf.pinMode(i, OUTPUT);      // Configurar los pines como salida
-  }
+void setup()
+{
+  Serial.begin(115200);                 // Iniciar puerto serie
+  Serial.println("INICIO DEL SISTEMA"); // Mensaje de inicio
 
-  // ************  comprobar pines como entradas **************
-  for (uint8_t i = 8; i < 16; i++) {  // Creamos un bucle para recorrer los pines de P10 a P17 (corresponden del 8 al 15)
-    pcf.pinMode(i, INPUT_PULLUP);        // Configuramos los pines como entrada
+  Wire1.begin(pinSDA, pinSCL, 400000); // Iniciar puerto I2C adicional
+  pcf.begin(0xFFFF);                   // Iniciar PCF con todos los pines en estado alto (0xFFFF = 1111 1111 1111 1111)
+}
+
+void loop()
+{
+  tiempoActual = millis();                        // Iniciamos el contador de tiempo
+  if (tiempoActual - tiempoAnterior >= intervalo) // Si ha pasado el tiempo del intervalo
+  {
+    tiempoAnterior = tiempoActual;                 // Actualizamos el tiempo anterior
+    leerPulsadores();                              // Leemos los pulsadores
+    for (uint8_t i = 0; i < cantidadEntradas; i++) // Recorremos todos los pulsadores
+    {
+      Serial.print(i);                  // Mostramos el número del pulsador
+      Serial.print(": ");               // Mostramos el número del pulsador
+      Serial.print(valorPulsadores[i]); // Mostramos el valor del pulsador
+      Serial.print("\t");               // Mostramos el número del pulsador
+    }
+    Serial.print(tiempoFinal - tiempoInicio); // Mostramos el tiempo que ha tardado en leer los pulsadores
+    Serial.println();                         // Salto de línea
   }
 }
 
-void loop() {
-  tiempoActual = millis();
-  for (uint8_t i = 8; i < 16; i++) {
-    if (pcf.digitalRead(i) == LOW) {
-      Serial.println("Pulsado el pin " + String(i));
-    }
-  }
+void leerPulsadores()
+{
+  tiempoInicio = micros(); // Iniciamos el contador de tiempo
 
-  if (tiempoActual - tiempoAnterior >= intervalo) {
-    tiempoAnterior = tiempoActual;
-    pcf.digitalWrite(numeroPin, !pcf.digitalRead(numeroPin));
-    Serial.println(numeroPin);  
-    numeroPin++;
-    if (numeroPin > 7) numeroPin = 0;     
+  uint16_t estadoTemp = pcf.getPinState(); // Leemos el estado de todos los pines de una vez
+
+  for (uint8_t i = 0; i < cantidadEntradas; i++) // Recorremos todos los bits
+  {
+    valorPulsadores[i] = (estadoTemp >> i) & 0x01; // Asignamos el valor del bit a una variable tipo bool
   }
-  delay(200);
+  
+  tiempoFinal = micros(); // Finalizamos el contador de tiempo
 }
